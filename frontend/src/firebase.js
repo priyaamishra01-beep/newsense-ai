7f5fc69de16b7699c1ba34877fb90c065c431477
Priya Mishra <priyaamishra.01@gmail.com>
2026-05-08 00:19:41 +0530

    Fixed Firebase storage bucket

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD5Ol8rN1VGIfb8DliZBkOYIr68Qr3jLeU",
   authDomain: "newsense-ai.firebaseapp.com",
   projectId: "newsense-ai",
-  storageBucket: "newsense-ai.appspot.com",
+   storageBucket: "newsense-ai.firebasestorage.app",
   messagingSenderId:"938982542435",
   appId: "1:938982542435:web:b4fe79997a894b8dc2bc56",
 };

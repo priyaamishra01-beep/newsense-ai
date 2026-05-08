e95ab9100cee65610493b4963657777ae02df078
Priya Mishra <priyaamishra.01@gmail.com>
2026-05-08 07:46:10 +0530

    Fixed Firebase auth config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,13 +1,18 @@
 import { initializeApp } from "firebase/app";
 
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5Ol8rN1VGIfb8DliZBkOYIr68Qr3jLeU",
   authDomain: "newsense-ai.firebaseapp.com",
   projectId: "newsense-ai",
-   storageBucket: "newsense-ai.firebasestorage.app",
-  messagingSenderId:"938982542435",
+  storageBucket: "newsense-ai.firebasestorage.app",
+  messagingSenderId: "938982542435",
   appId: "1:938982542435:web:b4fe79997a894b8dc2bc56",
 };
 
@@ -15,4 +20,10 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
+setPersistence(auth, browserLocalPersistence);
+
 export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
